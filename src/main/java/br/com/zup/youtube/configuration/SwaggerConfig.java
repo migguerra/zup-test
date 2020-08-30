24cab32330dc7a0dab528bc2af8d3d57c5707c8f
migguera <m_isadora02@hotmail.com>
2020-08-30 20:43:47 -0300

    alterar swagger documentations

--- src/main/java/br/com/zup/youtube/configuration/SwaggerConfig.java
+++ src/main/java/br/com/zup/youtube/configuration/SwaggerConfig.java
@@ -3,8 +3,10 @@ package br.com.zup.youtube.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -15,8 +17,14 @@ public class SwaggerConfig {
 
 	@Bean
 	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
-				.paths(PathSelectors.any()).build();
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("br.com.zup.youtube.controller")).paths(PathSelectors.any())
+				.build().apiInfo(metaData());
+	}
+
+	private ApiInfo metaData() {
+		return new ApiInfoBuilder().title("Youtube API").description("Documetations to Youtube API for Zup test")
+				.version("1.0.0").license("Apache Licence Version 2.0").build();
 	}
 
 }
--- src/main/java/br/com/zup/youtube/controller/ComentsController.java
+++ src/main/java/br/com/zup/youtube/controller/ComentsController.java
@@ -1,5 +1,7 @@
 package br.com.zup.youtube.controller;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,43 +18,74 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.zup.youtube.dto.ComentsDTO;
 import br.com.zup.youtube.service.ComentsService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 @RestController
+@Api(value = "coments", description = "Operations pertaining to Coments in API")
 @RequestMapping("/api/v1")
 public class ComentsController {
 
 	@Autowired
 	private ComentsService service;
 
-	@PostMapping(value = "/coments/{id-user}/{id-video}")
-	public ResponseEntity<ComentsDTO> saveComents(@RequestBody ComentsDTO comentsDto,
-			@PathVariable("id-user") Long idUser, @PathVariable("id-video") Long idVideo) {
+	private final String ERROR_MESSAGE_USER_ID = "USER ID NOT FOUND";
+
+	@ApiOperation(value = "Save a Coment", response = ComentsDTO.class)
+	@ApiResponses(value = { @ApiResponse(code = 201, message = "Coment Saved"),
+			@ApiResponse(code = 400, message = "The resource you were trying to reach is not found") })
+	@PostMapping(value = "/coments/{id-user}/{id-video}", produces = APPLICATION_JSON_VALUE, consumes = APPLICATION_JSON_VALUE)
+	public ResponseEntity<Object> saveComents(@RequestBody ComentsDTO comentsDto, @PathVariable("id-user") Long idUser,
+			@PathVariable("id-video") Long idVideo) {
+
 		ComentsDTO comentsSave = service.saveComentario(idUser, idVideo, comentsDto);
 
-		return new ResponseEntity<ComentsDTO>(comentsSave, HttpStatus.CREATED);
+		if (comentsSave == null) {
+			return new ResponseEntity<>(ERROR_MESSAGE_USER_ID, HttpStatus.BAD_REQUEST);
+		}
+
+		return new ResponseEntity<>(comentsSave, HttpStatus.CREATED);
 	}
 
-	@PutMapping(value = "/coments/{id}")
-	public ResponseEntity<ComentsDTO> updateComents(@PathVariable("id") Long id, @RequestBody ComentsDTO Coments) {
-		ComentsDTO updateComents = service.updateComentario(id, Coments);
+	@ApiOperation(value = "Updated a Coment", response = ComentsDTO.class)
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Coment Updated"),
+			@ApiResponse(code = 400, message = "The resource you were trying to reach is not found") })
+	@PutMapping(value = "/coments/{id}", produces = APPLICATION_JSON_VALUE, consumes = APPLICATION_JSON_VALUE)
+	public ResponseEntity<Object> updateComents(@PathVariable("id") Long id, @RequestBody ComentsDTO coments) {
 
-		return new ResponseEntity<ComentsDTO>(updateComents, HttpStatus.OK);
+		ComentsDTO updateComents = service.updateComentario(id, coments);
+
+		if (updateComents == null) {
+			return new ResponseEntity<>(ERROR_MESSAGE_USER_ID, HttpStatus.BAD_REQUEST);
+		}
+
+		return new ResponseEntity<>(updateComents, HttpStatus.OK);
 	}
 
+	@ApiOperation(value = "Deleted a Coment", response = ComentsDTO.class)
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Coment Deleted") })
 	@DeleteMapping(value = "/coments/{id}")
 	public ResponseEntity<Void> deleteComents(@PathVariable("id") Long id) {
 		service.deleteComent(id);
 		return ResponseEntity.ok().build();
 	}
 
-	@GetMapping(value = "/coments/video/{id-user}")
+	@ApiOperation(value = "Find Coments by a User Id", response = ComentsDTO.class)
+	@ApiResponses(value = { @ApiResponse(code = 201, message = "Retrived a List of Coments"),
+			@ApiResponse(code = 400, message = "The resource you were trying to reach is not found") })
+	@GetMapping(value = "/coments/video/{id-user}", produces = APPLICATION_JSON_VALUE)
 	public ResponseEntity<List<ComentsDTO>> findComentsByUser(@PathVariable("id-user") Long idUser) {
 		List<ComentsDTO> listComents = service.findByUser(idUser);
 
 		return ResponseEntity.ok(listComents);
 	}
 
-	@GetMapping(value = "/coments/user/{id-video}")
+	@ApiOperation(value = "Find Coments by a Video Id", response = ComentsDTO.class)
+	@ApiResponses(value = { @ApiResponse(code = 201, message = "Retrived a List of Coments"),
+			@ApiResponse(code = 400, message = "The resource you were trying to reach is not found") })
+	@GetMapping(value = "/coments/user/{id-video}", produces = APPLICATION_JSON_VALUE)
 	public ResponseEntity<List<ComentsDTO>> findComentsByVideo(@PathVariable("id-video") Long idVideo) {
 		List<ComentsDTO> listComents = service.findByVideo(idVideo);
 
